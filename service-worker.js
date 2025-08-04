d35227064ebce03bd2da62cc93024128460f6cbd
ErrorGuyWithGun <thevlad3323@gmail.com>
2025-08-05 01:11:01 +0300

    Deploy Blazor WASM with fixed Bootstrap

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-/* Manifest version: A+2kjtCL */
+/* Manifest version: U3RpS414 */
 // Caution! Be sure you understand the caveats before publishing an application with
 // offline support. See https://aka.ms/blazor-offline-considerations
 
